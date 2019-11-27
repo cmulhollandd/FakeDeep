d84201c40813e760b6f0ef6164a82314b4d70891
cmulhollandd <charliemul04@gmail.com>
2019-11-26 21:14:03 -0500

    updated README; added some more files

---
+++ ExtractFaces.py
@@ -0,0 +1,130 @@
+import os
+import sys
+import numpy as np
+import cv2
+from mtcnn.mtcnn import MTCNN
+
+def extract_face(img, detector, output_size=(64, 64)):
+    faces = detector.detect_faces(img)
+
+    if len(faces) == 0:
+        return None
+
+    keypoints = faces[0]["keypoints"]
+
+    pts = np.zeros((4, 2))
+
+    keys = list(keypoints.keys())
+    for i in range(0, len(keypoints) - 1):
+        pt = keypoints[keys[i]]
+        pts[i,0] = pt[0]
+        pts[i,1] = pt[1]
+
+
+    max_x = np.max(pts[:,0])
+    min_x = np.min(pts[:,0])
+    max_y = np.max(pts[:,1])
+    min_y = np.min(pts[:,1])
+
+    max_x += .5 * (max_x - min_x)
+    min_x -= .5 * (max_x - min_x)
+
+    max_y += .5 * (max_y - min_y)
+    min_y -= .5 * (max_y - min_y)
+
+    og_pts = np.array([[max_x, min_y], [max_x, max_y], [min_x, max_y], [min_x, min_y]], np.float32)
+    warped = np.array([[output_size[0], 0], [output_size[0], output_size[1]], [0, output_size[1]], [0, 0]], np.float32)
+
+    M = cv2.getPerspectiveTransform(og_pts, warped)
+
+    output = cv2.warpPerspective(img, M, output_size)
+
+    return output
+
+def loop_video(src, out, target_size=(64, 64), max_frames=None, show_frame=False):
+    cap = cv2.VideoCapture(src)
+    detector = MTCNN()
+
+    counter = 0
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            print("No frame found, exiting")
+            return
+
+        frame = cv2.resize(frame, (640, 360))
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        extracted = extract_face(img, detector, output_size=target_size)
+
+        if extracted is None:
+            continue
+
+        output = cv2.cvtColor(extracted, cv2.COLOR_RGB2BGR)
+
+        cv2.imwrite(os.path.join(out, "{0}.jpg".format(counter)), output)
+        print("{0}\r".format(counter), end='')
+        counter += 1
+        if show_frame:
+            cv2.imshow("output", extracted)
+            cv2.waitKey(1)
+
+        if max_frames is not None:
+            if counter >= max_frames:
+                print("{0} frames processed, exiting".format(max_frames))
+                return
+
+    return counter
+
+
+def process_videos(input_paths, target_size=(64, 64), max_frames=None, show_frame=False):
+    for i in range(0, len(input_paths)):
+        output_path = os.getcwd()
+        output_path = os.path.join(output_path, "faces")
+        output_path = os.path.join(output_path, "face{0}".format(i + 1))
+        output_path = os.path.join(output_path, 'face')
+        try:
+            os.mkdir(output_path)
+        except:
+            print("output {0} already exists, continuing".format(output_path))
+
+        loop_video(input_paths[i], output_path, target_size=target_size, max_frames=max_frames, show_frame=show_frame)
+
+def format_dirs():
+    current = os.getcwd()
+
+    faces = os.path.join(current, 'faces')
+    # os.mkdir(faces)
+    face1 = os.path.join(faces, "face1")
+    # os.mkdir(face1)
+    face2 = os.path.join(faces, "face2")
+    # os.mkdir(face2)
+    # os.mkdir(os.path.join(face2, "face"))
+    # os.mkdir(os.path.join(face1, "face"))
+
+    try:
+        os.mkdir(faces)
+        os.mkdir(face1)
+        os.mkdir(face2)
+        os.mkdir(os.path.join(face2, "face"))
+        os.mkdir(os.path.join(face1, "face"))
+    except:
+        print("output directories already exist, continuing")
+
+
+
+def main():
+    args = sys.argv
+
+    format_dirs()
+
+    input_paths = [args[1], args[2]]
+    max_frames = int(args[3])
+
+    process_videos(input_paths, target_size=(128, 128), max_frames=max_frames, show_frame=True )
+
+
+if __name__ == "__main__":
+    os.environ['KMP_DUPLICATE_LIB_OK']='True'
+    main()
---
+++ Train.py
@@ -0,0 +1,213 @@
+import os
+import sys
+import numpy as np
+import keras
+import cv2
+from keras.layers import *
+from keras.optimizers import Adamax, RMSprop
+from keras.models import Sequential, Model, load_model, model_from_json
+from keras.preprocessing.image import ImageDataGenerator
+
+def build_model(img_size=(64, 64), block_num=10):
+    print("building model")
+    if img_size != (64, 64):
+        input_layer = keras.Input(shape=(img_size[0], img_size[1], 3), name='input_layer')
+        x = Conv2D(64, 3, padding='same')(input_layer)
+        x = BatchNormalization()(x)
+        x = MaxPooling2D((2, 2))(x)
+
+        x = Conv2D(64, 3, padding='same')(x)
+        x = BatchNormalization()(x)
+        x = MaxPooling2D((2, 2))(x)
+
+        x = Conv2D(64, 3, padding='same')(x)
+        x = BatchNormalization()(x)
+        x = MaxPooling2D((2, 2))(x)
+
+        x = Conv2D(64, 3, padding='same')(x)
+        x = BatchNormalization()(x)
+        x = MaxPooling2D((2, 2))(x)
+
+        x = Conv2D(64, 3, padding='same')(x)
+        x = BatchNormalization()(x)
+        x = MaxPooling2D((2, 2))(x)
+
+        x = Conv2D(64, 1, padding='same', name='codings')(x)
+
+        x = Conv2DTranspose(64, 3, padding='same', strides=(2, 2))(x)
+        x = BatchNormalization()(x)
+
+        x = Conv2DTranspose(64, 3, padding='same', strides=(2, 2))(x)
+        x = BatchNormalization()(x)
+
+        x = Conv2DTranspose(64, 3, padding='same', strides=(2, 2))(x)
+        x = BatchNormalization()(x)
+
+        x = Conv2DTranspose(64, 3, padding='same', strides=(2, 2))(x)
+        x = BatchNormalization()(x)
+
+        x = Conv2DTranspose(64, 3, padding='same', strides=(2, 2))(x)
+        x = BatchNormalization()(x)
+
+        x = Conv2D(16, 3, padding='same')(x)
+        output = Conv2D(3, 3, padding='same')(x)
+
+        model = Model(input_layer, output)
+
+        model.summary()
+
+        return model
+
+    else:
+        json_path = os.path.join(os.getcwd(), "src")
+        json_path = os.path.join(json_path, 'dual_model.json')
+
+        model_architecture = ''
+        with open(json_path) as file:
+            model_architecture = file.read()
+
+        model = model_from_json(model_architecture)
+
+        model.summary()
+
+        return model
+
+def face_generator(face1_dir, face2_dir, batch_size=8, target_size=(64, 64), val_split=0.3, subset='none'):
+    face1_imgs = os.listdir(face1_dir)
+    face2_imgs = os.listdir(face2_dir)
+
+    faces = []
+
+    for path in face1_imgs:
+        faces.append(os.path.join(face1_dir, path))
+    for path in face2_imgs:
+        faces.append(os.path.join(face2_dir, path))
+
+
+    paths = np.array(faces)
+    np.random.shuffle(paths)
+
+    faces = paths.tolist()
+
+    if subset == 'none':
+        print()
+    elif subset == 'train':
+        faces = faces[int(val_split * len(faces)):]
+    elif subset == 'val':
+        faces = faces[:int(val_split * len(faces))]
+
+    paths = np.array(faces)
+    np.random.shuffle(paths)
+
+    num_faces = paths.shape[0]
+
+    print("Found {0} faces for {1}".format(num_faces, subset))
+
+    batch_counter = 0
+
+    while True:
+        batch_paths = []
+        if batch_counter + batch_size > num_faces:
+            batch_paths = faces[batch_counter:]
+            diff = batch_size - len(batch_paths)
+            batch_paths = np.append(batch_paths, faces[:diff - 1])
+            batch_counter = diff - 1
+        else:
+            batch_paths = faces[batch_counter: batch_counter + batch_size]
+            batch_counter += batch_size
+
+        batch_y = []
+
+        for path in batch_paths:
+            img = cv2.imread(path)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+            img = cv2.resize(img, target_size)
+
+            img = img / 255.
+
+            batch_y.append(img)
+
+        yield (np.array([batch_y, batch_y]))
+
+def train_pipeline(epochs, batch_size=8, img_size=(64, 64), val_split=0.3, lr=1e-2, loss='mse', visual=False):
+    if not visual:
+        model = None
+        if img_size != (64, 64):
+            model = build_model(img_size)
+        else:
+            model = build_model()
+
+        optimizer = Adamax(lr=lr, decay=lr/(epochs * .5))
+        model.compile(loss=loss, optimizer=optimizer)
+
+        face1_dir = os.path.join(os.getcwd(), "faces/face1/face")
+        face2_dir = os.path.join(os.getcwd(), "faces/face2/face")
+
+        val_size = int((len(os.listdir(face1_dir)) + len(os.listdir(face2_dir))) * val_split)
+        train_size = (val_size / val_split) * (1 - val_split)
+
+        train_gen = face_generator(face1_dir, face2_dir, subset='train', target_size=img_size, batch_size=batch_size, val_split=val_split)
+        val_gen = face_generator(face1_dir, face2_dir, subset='val', val_split=val_split, batch_size=batch_size, target_size=img_size)
+
+        print("Fitting model")
+        history = model.fit_generator(train_gen, steps_per_epoch=train_size//batch_size, validation_data=val_gen, validation_steps=val_size//batch_size)
+        print("Done")
+
+        model.save(os.path.join(os.getcwd(), "src/dual_model.h5"))
+    else:
+        model = None
+        if img_size != (64, 64):
+            model = build_model(img_size)
+        else:
+            model = build_model()
+
+        optimizer = Adamax(lr=lr, decay=lr/(epochs * .5))
+        model.compile(loss=loss, optimizer=optimizer)
+
+        face1_dir = os.path.join(os.getcwd(), "faces/face1/face")
+        face2_dir = os.path.join(os.getcwd(), "faces/face2/face")
+
+        val_size = int((len(os.listdir(face1_dir)) + len(os.listdir(face2_dir))) * val_split)
+        train_size = (val_size / val_split) * (1 - val_split)
+
+        train_gen = face_generator(face1_dir, face2_dir, subset='train', target_size=img_size, batch_size=batch_size, val_split=val_split)
+        val_gen = face_generator(face1_dir, face2_dir, subset='val', val_split=val_split, batch_size=batch_size, target_size=img_size)
+
+        train_steps = train_size // batch_size
+        val_steps = val_size // batch_size
+
+
+        print("Fitting model")
+        for epoch in range(0, epochs):
+            for train in range(0, int(train_steps)):
+                batch_x, batch_y = next(train_gen)
+                model.train_on_batch(batch_x, batch_y)
+                test = batch_x[-1]
+                sample = np.array([test])
+                pred = model.predict(sample)[0]
+                pred = pred - np.min(pred)
+                pred = pred / np.max(pred)
+                pred = np.array(pred * 255).astype('int8')
+                out = np.concatenate((np.array(test * 255).astype('int8'), pred), axis=1)
+                cv2.imshow('train image', out)
+                cv2.waitKey(1)
+            for val in range(0, int(val_steps)):
+                batch_x, batch_y = next(val_gen)
+                model.train_on_batch(batch_x, batch_y)
+                test = batch_x[-1]
+                sample = np.array([test])
+                pred = model.predict(sample)[0]
+                pred = pred - np.min(pred)
+                pred = pred / np.max(pred)
+                pred = np.array(pred * 255).astype('int8')
+                out = np.concatenate((np.array(test * 255).astype('int8'), pred), axis=1)
+                cv2.imshow('train image', out)
+                cv2.waitKey(1)
+
+        print("Done")
+
+        model.save(os.path.join(os.getcwd(), "src/dual_model.h5"))
+
+if __name__ == "__main__":
+    train_pipeline(10, batch_size=1, img_size=(128, 128), val_split=.2, lr=1e-3, visual=True)
